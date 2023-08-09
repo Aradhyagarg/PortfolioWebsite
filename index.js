859f007e9b3bcace36d77b3aa6d274facbed4ae9
Aradhya Garg <78140726+Aradhyagarg@users.noreply.github.com>
2023-08-09 15:36:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+// ========================================
+// creating a portfolio tabbed component
+// ========================================
+
+const p_btns = document.querySelector(".p-btns");
+const p_btn = document.querySelectorAll(".p-btn");
+const p_img_elem = document.querySelectorAll(".img-overlay");
+
+p_btns.addEventListener("click", (e) => {
+  const p_btn_clicked = e.target;
+  console.log(p_btn_clicked);
+
+  if (!p_btn_clicked.classList.contains("p-btn")) return;
+
+  p_btn.forEach((curElem) => curElem.classList.remove("p-btn-active"));
+
+  p_btn_clicked.classList.add("p-btn-active");
+
+  // to find the number in data attr
+  const btn_num = p_btn_clicked.dataset.btnNum;
+  console.log(btn_num);
+
+  const img_active = document.querySelectorAll(`.p-btn--${btn_num}`);
+
+  p_img_elem.forEach((curElem) => curElem.classList.add("p-image-not-acitve"));
+
+  img_active.forEach((curElem) =>
+    curElem.classList.remove("p-image-not-acitve")
+  );
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  spaceBetween: 30,
+  slidesPerView: "3",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  autoplay:{
+    delay:2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var typed = new Typed(".auto-type", {
+  strings: ["Hi👋", "I'm Aradhya Garg", "A B.E CSE Student"],
+  typeSpeed:150,
+  backSpeed:150,
+  loop:true
+});
